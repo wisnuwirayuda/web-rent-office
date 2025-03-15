e9a76949d241966b6ce5e75e577dc1239c3e676a
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2025-03-15 14:12:39 +0700

    Axios API Data Office

--- fe-rent-office/src/wrappers/BrowseOfficeWrapper.tsx
+++ fe-rent-office/src/wrappers/BrowseOfficeWrapper.tsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
 import OfficeCard from "../components/OfficeCard";
+import { Office } from "../types/type";
+import axios from "axios";
 
 export default function BrowseOfficeWrapper() {
+    const [offices, setOffices] = useState<Office[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/offices", {
+            headers: {
+                "X-API-KEY": "jky1377a8sd8a77821g3k2jlasdhk1a32e"
+            },
+        }).then((res) => {
+            setOffices(res.data.data);
+            setLoading(false);
+        }).catch((err) => {
+            setError(err);
+            setLoading(false);
+        })
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>Error loading data: {error}</p>
+    }
+
     return (
         <section
             id="Fresh-Space"
@@ -12,7 +41,9 @@ export default function BrowseOfficeWrapper() {
                 For Your Better Productivity.
             </h2>
             <div className="grid grid-cols-3 gap-[30px]">
-                <OfficeCard></OfficeCard>
+                {offices.map((office) => (
+                    <OfficeCard key={office.id} office={office}></OfficeCard>
+                ))}
             </div>
         </section>
     )
