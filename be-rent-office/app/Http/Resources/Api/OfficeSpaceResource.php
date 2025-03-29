e2dc08a070a03bb60ebc5908aeaef38537371aa2
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2025-03-30 01:39:23 +0700

    Menampilkan Details Data Office

--- be-rent-office/app/Http/Resources/Api/OfficeSpaceResource.php
+++ be-rent-office/app/Http/Resources/Api/OfficeSpaceResource.php
@@ -17,6 +17,7 @@ class OfficeSpaceResource extends JsonResource
         return [
             'id'            => $this->id,
             'name'          => $this->name,
+            'address'       => $this->address,
             'slug'          => $this->slug,
             'duration'      => $this->duration,
             'price'         => $this->price,
