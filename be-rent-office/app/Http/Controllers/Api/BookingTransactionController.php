d37a6f99f119114d730d2d406bd07ba90d892556
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2025-03-31 01:15:44 +0700

    Coding Fitur Check Booking Transaction

--- be-rent-office/app/Http/Controllers/Api/BookingTransactionController.php
+++ be-rent-office/app/Http/Controllers/Api/BookingTransactionController.php
@@ -6,11 +6,31 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreBookingTransactionRequest;
 use App\Models\BookingTransaction;
 use App\Http\Resources\Api\BookingTransactionResource;
+use App\Http\Resources\Api\ViewBookingResource;
 use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
+    public function booking_details(Request $request)
+    {
+        $request->validate([
+            'phone_number'      => 'required|string',
+            'booking_trx_id'    => 'required|string'
+        ]);
+
+        $booking = BookingTransaction::where('phone_number', $request->phone_number)
+            ->where('booking_trx_id', $request->booking_trx_id)
+            ->with(['officeSpace', 'officeSpace.city'])
+            ->first();
+
+        if (!$booking) {
+            return response()->json(['message' => 'Booking not found'], 404);
+        }
+
+        return new ViewBookingResource($booking);
+    }
+
     public function store(StoreBookingTransactionRequest $request) 
     {
         $validateData = $request->validated();
